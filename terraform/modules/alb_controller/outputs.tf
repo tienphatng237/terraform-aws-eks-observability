18d19f85b4595c4fcd91d0765f3faef4938a954b
tienphatng237 <phat48419@gmail.com>
2025-09-25 08:28:51 +0700

    chore(alb-controller): add output for IAM role ARN

--- terraform/modules/alb_controller/outputs.tf
+++ terraform/modules/alb_controller/outputs.tf
@@ -1,4 +1,4 @@
-output "alb_controller_role_arn" { 
-    value = aws_iam_role.alb_sa.arn 
-    }
-
+output "alb_controller_role_arn" {
+  description = "IAM Role ARN for AWS Load Balancer Controller"
+  value       = aws_iam_role.alb_sa.arn
+}
