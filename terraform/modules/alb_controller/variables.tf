f2add58f6eaa436c609d37ac4501d1731490e405
tienphatng237 <phat48419@gmail.com>
2025-09-25 08:28:28 +0700

    chore(alb-controller): define input variables for the module

--- terraform/modules/alb_controller/variables.tf
+++ terraform/modules/alb_controller/variables.tf
@@ -1,24 +1,30 @@
-variable "project_name" { 
-    type = string 
-    }
+variable "project_name" {
+  type        = string
+  description = "Project name used for tagging and naming resources"
+}
 
-variable "region" { 
-    type = string 
-    }
+variable "region" {
+  type        = string
+  description = "AWS region where the resources will be created"
+}
 
-variable "cluster_name" { 
-    type = string 
-    }
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name"
+}
 
-variable "vpc_id" { 
-    type = string 
-    }
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for the EKS cluster"
+}
 
-variable "oidc_provider_arn" { 
-    type = string 
-    }
+variable "oidc_provider_arn" {
+  type        = string
+  description = "OIDC provider ARN from the EKS cluster"
+}
 
-variable "namespace" { 
-    type = string
-    default = "kube-system" 
-    }
+variable "namespace" {
+  type        = string
+  default     = "kube-system"
+  description = "Namespace for AWS Load Balancer Controller"
+}
