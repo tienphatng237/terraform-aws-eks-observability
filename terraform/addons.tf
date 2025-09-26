aa81707a622575771efa15fa0abb94b4953630b7
tienphatng237 <phat48419@gmail.com>
2025-09-26 14:36:37 +0700

    fix(terraform/eks): configure EBS CSI driver with Pod Identity
    - Replace invalid trust policy (eks.amazonaws.com) with Pod Identity (pods.eks.amazonaws.com)
    - Add IAM role and policy attachment for AmazonEBSCSIDriverPolicy
    - Introduce aws_eks_pod_identity_association binding to 'ebs-csi-controller-sa' in kube-system
    - Ensure addon creation waits for EKS cluster (depends_on = [module.eks])
    - This fixes provisioning errors during terraform apply for the aws-ebs-csi-driver addon

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -1,19 +1,17 @@
+# AWS EBS CSI Addon
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name                = var.cluster_name
   addon_name                  = "aws-ebs-csi-driver"
   addon_version               = "v1.29.0-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
-  service_account_role_arn    = aws_iam_role.ebs_csi_driver_role.arn
 
-    depends_on = [
-    module.eks,
-    aws_iam_role.ebs_csi_driver_role
-  ]
+  depends_on = [ module.eks ]
 }
 
-resource "aws_iam_role" "ebs_csi_driver_role" {
-  name = "${var.cluster_name}-ebs-csi-driver-role"
+# IAM policy for Pod Identity
+resource "aws_iam_role" "ebs_csi_pod_role" {
+  name = "${var.cluster_name}-ebs-csi-pod-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -23,13 +21,23 @@ resource "aws_iam_role" "ebs_csi_driver_role" {
         Principal = {
           Service = "eks.amazonaws.com"
         }
-        Action = "sts:AssumeRole"
+        Action = [ "sts:AssumeRole", "sts:TagSession" ]
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
   role       = aws_iam_role.ebs_csi_driver_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+# Link role with SA ebs-csi-controller-sa
+resource "aws_eks_pod_identity_association" "ebs_csi_assoc" {
+  cluster_name    = var.cluster_name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi_pod_role.arn
+
+  depends_on = [ aws_eks_addon.ebs_csi ]
 }
