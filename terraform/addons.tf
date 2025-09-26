6d5795182e5d34523b958e53b8b12d9a07e219a8
tienphatng237 <phat48419@gmail.com>
2025-09-26 15:48:20 +0700

    fix(terraform/eks): correct IAM role reference for EBS CSI driver
    - Changed aws_iam_role_policy_attachment to reference aws_iam_role.ebs_csi_pod_role
    - Previous name 'ebs_csi_driver_role' was undeclared and caused terraform plan failure
    - Ensures IAM role policy attachment is bound to the correct Pod Identity role
    - Allows successful provisioning of AmazonEBSCSIDriverPolicy for the EBS CSI addon

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role" "ebs_csi_pod_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
-  role       = aws_iam_role.ebs_csi_driver_role.name
+  role       = aws_iam_role.ebs_csi_pod_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
