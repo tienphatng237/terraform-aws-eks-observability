340594620f1e3834d753ce427e522ce0ffc4b8ac
tienphatng237 <phat48419@gmail.com>
2025-09-27 10:22:11 +0700

    fix(terraform/eks): let AWS auto-select EBS CSI addon version
    - Removed hardcoded addon_version from aws_eks_addon.ebs_csi
    - Avoids InvalidParameterException when cluster version > addon version
    - Ensures latest supported EBS CSI driver is provisioned for EKS 1.30
    - Simplifies addon management and future upgrades

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -2,7 +2,6 @@
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name                = var.cluster_name
   addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.29.0-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
