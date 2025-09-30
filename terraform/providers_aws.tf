330637c3db824eedfb0d8ea3816789604b51e5fd
tienphatng237 <phat48419@gmail.com>
2025-09-30 20:08:40 +0700

    feat(providers): add providers for EKS cluster

---
+++ terraform/providers_aws.tf
@@ -0,0 +1,4 @@
+# terraform/providers_aws.tf
+provider "aws" {
+  region = var.region
+}
--- terraform/providers_kube.tf
+++ terraform/providers_kube.tf
@@ -1,11 +1,11 @@
 # Data sources referencing the EKS cluster created by module.eks
 data "aws_eks_cluster" "this" {
-  name       = module.eks.cluster_name
+  name      = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name       = module.eks.cluster_name
+  name      = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
@@ -16,7 +16,6 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-# Helm provider - reuses the same connection info
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.this.endpoint
